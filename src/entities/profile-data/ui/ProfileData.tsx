df8e8228c756b974edbc2fa1a2d81ffe4ccb6bb5
lomeiko-dev <lomieiko78@mail.ru>
2023-09-20 22:42:38 +0300

    optimization code

--- src/entities/profile-data/ui/ProfileData.tsx
+++ src/entities/profile-data/ui/ProfileData.tsx
@@ -1,54 +1,57 @@
-import React, {useEffect} from "react";
+import React from "react";
 import style from "./ProfileData.module.scss";
-
-import {useAppSelector} from "shared/lib/hooks/use-app-selector/useAppSelector";
-import {useAppDispatch} from "shared/lib/hooks/use-app-dispatch/useAppDispatch";
-
-import {ReducerLoader} from "shared/ui/reducer-loader/ReducerLoader";
 import {Text, textStyled} from "shared/ui/text/Text";
+import {IProfileData} from "../model/typs/profile-data-scheme";
 
-import {profileDataReducer} from "../model/slice/profile-data-slice";
-import {getProfileDataThunk} from "../model/services/get-profile-data-thunk";
-import {profileDataSelector} from "../model/selectors/profile-data/profile-data-selector";
-import {errorSelector} from "../model/selectors/error/error-selector";
+import loader from "shared/assets/gif/loaders/loader.gif";
+import classNames from "classnames";
 
 const TFILE: string = "profile_data";
 
-export const ProfileData = React.memo(() => {
-    const dispatch = useAppDispatch();
+interface IProfileDataProps{
+    className?: string,
+    data?: IProfileData,
+    isLoading: boolean,
+    error?: string,
+}
 
-    const profileData = useAppSelector(profileDataSelector);
-    const error = useAppSelector(errorSelector);
+export const ProfileData: React.FC<IProfileDataProps> = React.memo((props) => {
+    const {
+        className,
+        data,
+        error,
+        isLoading
+    } = props;
+    let content: React.ReactNode;
 
-    useEffect(() => {
-        if(profileData === undefined)
-            dispatch(getProfileDataThunk());
-    }, []);
+    if(error !== undefined)
+        content = <Text styled={textStyled.ERROR}>{error}</Text>
+    if(isLoading)
+        content = <img src={loader} alt="load"/>
 
     return (
-        <ReducerLoader storeKey="profileDataReducer" reducer={profileDataReducer} save={true}>
-            <div className={style.data}>
-                {profileData !== undefined ? (
+            <div className={classNames(style.data, className)}>
+                {data && (
                     <>
                         <div className={style.block}>
                             <Text tfile={TFILE} tkey="age"/>
-                            <Text>{profileData.age}</Text>
+                            <Text>{data.age}</Text>
                         </div>
 
                         <div className={style.block}>
                             <Text tfile={TFILE} tkey="education"/>
-                            <Text>{profileData.education}</Text>
+                            <Text>{data.education}</Text>
                         </div>
 
                         <div className={style.block}>
                             <Text tfile={TFILE} tkey="position"/>
-                            <Text>{profileData.position}</Text>
+                            <Text>{data.position}</Text>
                         </div>
 
-                        <Text className={style.bio_block}>{profileData.biography}</Text>
+                        <Text className={style.bio_block}>{data.biography}</Text>
                     </>
-                ) : <Text styled={textStyled.ERROR}>{error}</Text>}
+                )}
+                {content}
             </div>
-        </ReducerLoader>
     )
 });
--- src/page/main/ui/MainPage.tsx
+++ src/page/main/ui/MainPage.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {useEffect} from "react";
 import style from "./MainPage.module.scss";
 
-import {ProfileData, profileDataReducer} from "entities/profile-data";
+import {getProfileDataThunk, ProfileData, profileDataReducer} from "entities/profile-data";
 import {Panel} from "shared/ui/panel/Panel";
 import {ReducerLoader} from "shared/ui/reducer-loader/ReducerLoader";
 
 import {errorSelector, isloadingSelector, Profile, profileSelector} from "entities/profile";
+
+import {errorSelector as profileDataErrorSelector,
+        isLoadingSelector as profileDataIsLoadingSelector,
+        profileDataSelector} from "entities/profile-data";
+
 import {useAppSelector} from "shared/lib/hooks/use-app-selector/useAppSelector";
+import {useAppDispatch} from "shared/lib/hooks/use-app-dispatch/useAppDispatch";
 import {avatarStyled} from "entities/profile/ui/profile/Profile";
 
 const MainPage = () => {
+    const dispatch = useAppDispatch();
+
     const profile = useAppSelector(profileSelector);
     const isLoading = useAppSelector(isloadingSelector);
     const error = useAppSelector(errorSelector);
+
+    const profileData = useAppSelector(profileDataSelector);
+    const profileDataIsLoading = useAppSelector(profileDataIsLoadingSelector);
+    const profileDataError = useAppSelector(profileDataErrorSelector);
+
+    useEffect(() => {
+        if(profileData === undefined)
+            dispatch(getProfileDataThunk());
+    }, [profileData]);
 
     return (
         <ReducerLoader save={true} storeKey="profileDataReducer" reducer={profileDataReducer}>
@@ -24,7 +41,9 @@ const MainPage = () => {
                         data={profile} isLoading={isLoading} error={error}
                     />
                 </Panel>
-                <ProfileData/>
+                <ProfileData
+                    data={profileData} isLoading={profileDataIsLoading} error={profileDataError}
+                />
             </div>
         </ReducerLoader>
     )
